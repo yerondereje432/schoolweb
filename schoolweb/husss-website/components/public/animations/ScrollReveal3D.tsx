ed784ccf93a99c54dbb957e513abad1f0ac09106
Yeron Dereje <yerondereje432@gmail.com>
2026-07-18 22:50:10 +0300

    Import React alongside other hooks in ScrollReveal3D

--- schoolweb/husss-website/components/public/animations/ScrollReveal3D.tsx
+++ schoolweb/husss-website/components/public/animations/ScrollReveal3D.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState, type ReactNode } from "react";
+import React, { useEffect, useRef, useState, type ReactNode } from "react";
 
 type AnimationType =
   | "fade-up"
